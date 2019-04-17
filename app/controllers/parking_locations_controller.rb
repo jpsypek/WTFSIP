391c8c2d4b7a7e143f80a8182cc33635ce4f4553
github username <github email address>
2019-04-17 10:36:26 -0600

    connect classes in parking event form

--- app/controllers/parking_locations_controller.rb
+++ app/controllers/parking_locations_controller.rb
@@ -4,6 +4,7 @@ class ParkingLocationsController < ApplicationController
   def index
     if params[:search_street]
       @parking_locations = ParkingLocation.where("name LIKE ?", "%#{params[:search_street]}%")
+      @user = User.find(params[:user_id])
     else
       @parking_locations = ParkingLocation.all
     end
@@ -11,16 +12,20 @@ class ParkingLocationsController < ApplicationController
 
   def show
     @parking_event = ParkingEvent.new
+    @destinations = Destination.all
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+    end
   end
 
-  # def create_parking_event
-  #   # byebug
-  #   @parking_event = @parking_location.parking_events.build(parking_location_params)
-  #   if @parking_event.save
-  #     @parking_event = ParkingEvent.new
-  #   end
-  #   render :show
-  # end
+  def create_parking_event
+    # byebug
+    @parking_event = @parking_location.parking_events.build(parking_location_params)
+    if @parking_event.save
+      @parking_event = ParkingEvent.new
+    end
+    render :show
+  end
 
   private
 
@@ -29,6 +34,6 @@ class ParkingLocationsController < ApplicationController
   end
 
   def parking_location_params
-    params.require(:parking_location).permit(:name, :latitude, :longitude) #parking_events_attributes:[:day, :time, :user_id, :destination_id, :parking_location_id, :ease_rating, :safety_rating, :time_spent, :walkability_rating, :notes])
+    params.require(:parking_location).permit(:name, :latitude, :longitude, parking_events_attributes:[:day, :time, :user_id, :destination_id, :parking_location_id, :ease_rating, :safety_rating, :time_spent, :walkability_rating, :notes])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  resources :parking_events
+
   resources :location_limitations
   resources :users
   resources :limitations
   resources :parking_locations do
-    member do
-      post 'create_parking_event'
+    resources :parking_events
     end
-  end
+  
   resources :destinations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: "users#index"
